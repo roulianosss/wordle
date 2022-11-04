96323d563f0a5b9bed19640dcfd1089c91173c30
Roulianosss <julien.ortscheid@gmail.com>
2022-11-04 09:52:31 +0100

    change link fetch

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://roulianos:bikett@cluster0.okzcmrm.mongodb.net/wordle';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
